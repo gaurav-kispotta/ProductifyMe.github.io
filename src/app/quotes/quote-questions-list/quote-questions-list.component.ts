e0e841ca71f8de2aa3421c1c9f3186dc8a38ecb3
RaveenaGumba <RaveenaGumba@users.noreply.github.com>
2020-05-28 00:41:57 +0530

    Commit1
    :)

--- src/app/quotes/quote-questions-list/quote-questions-list.component.ts
+++ src/app/quotes/quote-questions-list/quote-questions-list.component.ts
@@ -13,16 +13,34 @@ export class QuoteQuestionsListComponent implements OnInit {
   constructor() {
 
     const q = new QuoteQuestions();
-    q.questionTitle = 'Select total CPUs ?';
+    q.questionTitle = 'Asset Life ?';
     q.minCount = 0;
     q.maxCount = 10;
     q.value = 0;
 
+    const q1 = new QuoteQuestions();
+    q1.questionTitle = 'Initial Capacity Requirement ?';
+    q1.minCount = 0;
+    q1.maxCount = 10;
+    q1.value = 0;
+
+    const q2 = new QuoteQuestions();
+    q2.questionTitle = 'Annual Growth ?';
+    q2.minCount = 0;
+    q2.maxCount = 10;
+    q2.value = 0;
+
+    const q3 = new QuoteQuestions();
+    q3.questionTitle = 'Migration Period ?';
+    q3.minCount = 0;
+    q3.maxCount = 10;
+    q3.value = 0;
+
 
     this.quoteQuestionList.push(q);
-    this.quoteQuestionList.push(q);
-    this.quoteQuestionList.push(q);
-    this.quoteQuestionList.push(q);
+    this.quoteQuestionList.push(q1);
+    this.quoteQuestionList.push(q2);
+    this.quoteQuestionList.push(q3);
    }
 
   ngOnInit() {
